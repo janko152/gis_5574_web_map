edbf3e970265fbe409772f6ad2f55cc72a925437
Krystal Jankowski <janko152@umn.edu>
2025-11-09 23:28:36 -0600

    Add files via upload

---
+++ web_map_functions.js
@@ -0,0 +1,12 @@
+require(["esri/views/MapView", "esri/WebMap"], (MapView, WebMap) => {
+  const webmap = new WebMap({
+    portalItem: {
+      id: "320022aa62d14940a577fcba787fcf86"
+    }
+  });
+
+  const view = new MapView({
+    map: webmap,
+    container: "viewDiv"
+  });
+});
