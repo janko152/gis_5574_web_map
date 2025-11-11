5f72b33c33f7e5ccd614cc129b0faa6019811b71
Krystal Jankowski <janko152@umn.edu>
2025-11-11 15:16:22 -0600

    Integrate Legend and Search widgets into map
    Added Legend and Search widgets to the map view.

--- web_map_functions.js
+++ web_map_functions.js
@@ -1,4 +1,4 @@
-require(["esri/config", "esri/views/MapView", "esri/WebMap"], function (esriConfig, MapView, WebMap) {
+require(["esri/config", "esri/views/MapView", "esri/WebMap", "esri/widgets/Legend", "esri/widgets/Search"], function (esriConfig, MapView, WebMap, Legend, Search) {
   
   const webmap = new WebMap({
     portalItem: {
@@ -11,4 +11,18 @@ require(["esri/config", "esri/views/MapView", "esri/WebMap"], function (esriConf
     container: "viewDiv"
   });
   
+  let legend = new Legend ({
+    view: view
+  });
+  
+  view.ui.add(legend, "top-right")
+  
+  const searchWidget = new Search ({
+    view: view
+  });
+  
+  view.ui.add(searchWidget, {
+    position: "bottom-left",
+    index: 2
+  });
 });
